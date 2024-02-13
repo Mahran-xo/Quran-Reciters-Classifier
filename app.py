79b402c528553ad9916e5cd97985a303638505de
Mahran-xo <ali_mohamed2001@hotmail.com>
2024-02-13 18:12:17 +0200

    (New)

--- app.py
+++ app.py
@@ -1,19 +1,54 @@
 from pydub import AudioSegment
-from fastapi import FastAPI,File,UploadFile
+from fastapi import FastAPI
 import random
 import numpy as np
+import os
+import shutil
 import tensorflow as tf
 from pytube import YouTube
 from pydub import AudioSegment
 import uvicorn
-# import arabic_reshaper
-# from bidi.algorithm import get_display
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-# Example usage
-# video_url = 'https://www.youtube.com/watch?v=K7dz7JaPtoo'
+
+# Allow all origins, methods, and headers. Adjust these ssettings based on your requirements.
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:8000/docs#",
+    "http://localhost:3000",  # Example for a React app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 output_path = './output_folder'
 imported = tf.saved_model.load("saved")
+
+def remove_all_content(directory_path):
+    try:
+        # Iterate over all items in the directory
+        for item in os.listdir(directory_path):
+            item_path = os.path.join(directory_path, item)
+
+            # Check if it's a file or directory
+            if os.path.isfile(item_path):
+                # Remove file
+                os.remove(item_path)
+            elif os.path.isdir(item_path):
+                # Remove directory and its contents recursively
+                shutil.rmtree(item_path)
+
+        print(f"All content in {directory_path} has been removed.")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def download_youtube_audio(video_url, output_path, output_format='wav'):
@@ -44,13 +79,10 @@ def extract_random_second(input_audio_path, output_audio_path):
     one_second_audio.export(audio_path_out, format="wav")
 
 
-@app.post("predict/")
-async def predict(url:str):
-
-    INPUT = download_youtube_audio(url, output_path)
+@app.post("/predict/")
+async def predict(link:str):
+    INPUT = download_youtube_audio(link, output_path)
     extract_random_second(INPUT, INPUT)
-
-    
     pred = imported(INPUT)
     class_names=pred['class_names']
     class_id=np.argmax(pred['class_ids'])
@@ -58,7 +90,7 @@ async def predict(url:str):
 
     label =final_pred.numpy().decode('utf-8')
 
-
+    remove_all_content(output_path)
     return {"prediction": label}
 
 if __name__ == "__main__":
